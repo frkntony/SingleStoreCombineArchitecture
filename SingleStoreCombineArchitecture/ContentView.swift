cc1816da2d15f91775d442b2fccaf5aa0b94e21e
frkntony <frkntony@linux.com>
2020-01-04 01:40:57 +0300

    Initial Commit

---
+++ SingleStoreCombineArchitecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SingleStoreCombineArchitecture
+//
+//  Created by Anton Elistratov on 04.01.2020.
+//  Copyright © 2020 Anton Elistratov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
